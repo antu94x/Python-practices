5454a346636d7879fa4e39f0b2cac16ac277f2b7
ALMIGHTY <antud137@gmail.com>
2026-07-12 01:24:38 +0600

    Update Practice - 12.py

--- Practice - 12.py
+++ Practice - 12.py
@@ -1,3 +1,4 @@
-var1=int(input("Enter first number ="))
-var2=int(input("Enter second number ="))
+var1=int(input("Enter first number ="))#first number
+var2=int(input("Enter second number ="))#second number
+
 print(var1%var2)
