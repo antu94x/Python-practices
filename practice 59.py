e749b73d36c94103dc30338bf125a67eafc980bb
ALMIGHTY <antud137@gmail.com>
2026-07-11 22:04:08 +0600

    Add files via upload

---
+++ practice 59.py
@@ -0,0 +1,15 @@
+i=1
+
+print("using while loop")
+
+while i<=10:#while loop
+    print(i, end="   ")
+    i+=1
+    
+    
+print("")
+
+print("using for loop")
+
+for i in range (1,11):#for loop
+    print(i, end= "     ")
