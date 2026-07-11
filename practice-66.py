5651c881be07332338ace400e877fd857e30c445
ALMIGHTY <antud137@gmail.com>
2026-07-12 01:15:34 +0600

    Practice 66

---
+++ practice-66.py
@@ -0,0 +1,18 @@
+N=int(input("Enter number for odd numbers sum =",))
+
+total=0
+#Using while loop
+
+i=1
+while i<=10:
+    total+=i
+    i+=2
+
+print("Using while loop 1 to",N,"odd number sum =",total)
+
+#Using for loop
+for i in range(1,N+1,2):
+    total+=i
+    
+print(total)
+
