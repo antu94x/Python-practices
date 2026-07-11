69a95a42b8d516d8d92100dd9e0b1cc72f5c222c
ALMIGHTY <antud137@gmail.com>
2026-07-11 22:20:05 +0600

    Add files via upload

---
+++ practice 63.py
@@ -0,0 +1,18 @@
+print("in for loop")
+total=0
+
+for i in range(1,101):
+    
+    total=total+i
+print("Total is",total)
+
+print("in while loop")
+
+i=1
+Total=0
+
+while i<=100:
+    
+    Total+=i
+    i+=1
+print("Total is ",Total)
